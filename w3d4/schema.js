bc0567ab542a40a7689747ca68cb02c04421aedf
Dave Brown <d.boyer.brown@gmail.com>
2024-03-22 23:03:14 -0400

    finish

---
+++ w3d4/schema.js
@@ -0,0 +1,34 @@
+class User {
+    userId;
+    username;
+    password;
+    firstName;
+    lastName;
+    role;
+}
+
+class Post {
+    postId;
+    subject;
+    body;
+    date;
+
+}
+
+class UserPost {
+    userPostId;
+    postId;
+    userId;
+}
+
+class Comments {
+    commentId
+    body;
+    date;
+}
+
+class PostComments {
+    postCommentId
+    postId;
+    commentIt;
+}
---
+++ w3d4/schema.py
@@ -0,0 +1,31 @@
+class User:
+    username
+    password
+    firstName
+    lastName
+    role
+
+
+class Post:
+    postId
+    subject
+    body
+    date
+
+
+
+class UserPost:
+    userPostId
+    postId
+    userId
+
+class Comments:
+    commentId
+    body
+    date
+
+
+class PostComments:
+    postCommentId
+    postId
+    commentIt
